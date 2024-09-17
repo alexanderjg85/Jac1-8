c1a39fbed1cc433741e96e36282be6af7468bfad
Alexander Jaeger <alexanderjg85@gmail.com>
2024-09-17 23:31:29 +0200

    Added Assertions in Registerset TB
    Signed-off-by: Alexander Jaeger <alexanderjg85@gmail.com>

--- src/registerset_tb.v
+++ src/registerset_tb.v
@@ -34,37 +34,64 @@ initial begin
     reg_in = 0;
     #10
     res_n = 1;
-    #10
+    #6 //Nach dem Reset soll 0 in den Ausgangsregistern stehen
+    assert(reg_out_1 === 0);
+    assert(reg_out_2 === 0);
+    #4 //Schreibe 255 in R3, => R0 = 0, R1 = 0, R2 = 0, R3 = 255
     reg_in = 255;
     wr_sel = 3;
     wr_en = 1;
-    #10
+    #6 //Ausgang darf sich nicht verändern
+    assert(reg_out_1 === 0);
+    assert(reg_out_2 === 0);
+    #4  //Schreibe 15 in R2, => R0 = 0, R1 = 0, R2 = 15, R3 = 255
     reg_in = 15;
     wr_sel = 2;
-    #10
+    #6 //Ausgang darf sich nicht verändern
+    assert(reg_out_1 === 0);
+    assert(reg_out_2 === 0);
+    #4  //Schreibe 128 in R0, => R0 = 128, R1 = 0, R2 = 15, R3 = 255
     reg_in = 128;
     wr_sel = 0;
-    #10
+    #6 //Ausgang darf sich nicht verändern
+    assert(reg_out_1 === 0);
+    assert(reg_out_2 === 0);
+    #4  //Lese R3 = 255, => R0 = 128, R1 = 0, R2 = 15, R3 = 255
     wr_en = 0;
     rd_en1 = 1;
     rd_sel1 = 3;
-    #10
+    #6 //Reg_out 1 = R3 = 255
+    assert(reg_out_1 === 255);
+    assert(reg_out_2 === 0);
+    #4 //NOP - Lese und Schreibe nichts, Ausgänge müssen 0 sein
     rd_en1 = 0;
     rd_sel1 = 0;
-    #10
+    #6
+    assert(reg_out_1 === 0);
+    assert(reg_out_2 === 0);
+    #4 //Schreibe 3 in R1 und Lese R1 an Out2 = 128, => R0 = 128, R1 = 3, R2 = 15, R3 = 255
     wr_en = 1;
     wr_sel = 1;
     reg_in = 3;
     rd_en2 = 1;
-    #10
+    #6 //Ausgang 2 muss 128 sein
+    assert(reg_out_1 === 0);
+    assert(reg_out_2 === 128);
+    #4 //Lese R2 an Out1 = 15 und Lese R2 an Out2 = 15, => R0 = 128, R1 = 3, R2 = 15, R3 = 255
     wr_en = 0;
     rd_en1 = 1;
     rd_sel1 = 2;
     rd_sel2 =2;
-    #10
+    #6 //Ausgang 1 und 2 müssem 15 sein
+    assert(reg_out_1 === 15);
+    assert(reg_out_2 === 15);
+    #4 //Lese R1 an Out1 = 3 und Lese R3 an Out2 = 255, => R0 = 128, R1 = 3, R2 = 15, R3 = 255
     rd_sel1 = 1;
     rd_sel2 = 3;
-    #10
+    #6
+    assert(reg_out_1 === 3);
+    assert(reg_out_2 === 255);
+    #4
 
 	$finish();
 end
